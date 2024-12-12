a435724611df430699057c7673735981efd9ee9d
Millie Houston <Immaloser102@gmail.com>
2024-12-12 14:44:47 -0800

    Currently working on the Date and task 4

--- components/Todo.js
+++ components/Todo.js
@@ -5,17 +5,30 @@ class Todo {
     }
 
     _setEventListeners () {
-        //set 'change' on checkboxEL
-        
+        this._todoCheckboxEl.addEventListener("change", () => {
+            this._data.completed = !this._data.completed;
+        });
+        //when clicked, change completion from true to false
+        this._todoDeleteBtn.addEventListener("click", () => {
+            this._todoElement.remove();
+        });
+
     }
 
+    _dateEl () {
+        //Add the date stuff from index.js
+    }
+
+    _deleteEl () {
+       this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+    }
 
     _generateCheckboxEl () {
-        const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
-        const todoLabel = this._todoElement.querySelector(".todo__label");
-        todoCheckboxEl.checked = this._data.complete
-        todoCheckboxEl.id = `todo-${this._data.id}`;
-        todoLabel.setAttribute("for", `todo-${this._data.id}`);
+        this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+        this._todoLabel = this._todoElement.querySelector(".todo__label");
+        this._todoCheckboxEl.checked = this._data.complete
+        this._todoCheckboxEl.id = `todo-${this._data.id}`;
+        this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
     }
 
     getView() {
@@ -31,6 +44,10 @@ class Todo {
         todoNameEl.textContent = this._data.name;
         
         this._generateCheckboxEl();
+
+        this._deleteEl();
+
+        this._setEventListeners();
 
         return this._todoElement;
     }
