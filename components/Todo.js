3b262d9fb0e7368d7bea84bb7e3f678f3123a600
Millie Houston <Immaloser102@gmail.com>
2024-12-12 13:46:10 -0800

    a little behind

--- components/Todo.js
+++ components/Todo.js
@@ -1,25 +1,32 @@
 class Todo {
-    constructor(data, templateSelector) {
+    constructor(data, selector) {
         this._data = data;
-        this._templateElement = document.querySelector(templateSelector);
+        this._templateElement = document.querySelector(selector);
     }
 
     getView() {
-        this,_todoElement = this._templateElement.content
+        this._todoElement = this._templateElement.content
           .querySelector(".todo")
           .cloneNode(true);
-        const todoNameEl = todoElement.querySelector(".todo__name");
-        const todoCheckboxEl = todoElement.querySelector(".todo__completed");
-        const todoLabel = todoElement.querySelector(".todo__label");
-        const todoDate = todoElement.querySelector(".todo__date");
-        const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
+
+          const todoNameEl = this._todoElement.querySelector(".todo__name");
+          const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+          const todoLabel = this._todoElement.querySelector(".todo__label");
+          const todoDate = this._todoElement.querySelector(".todo__date");
+          const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+          
 
         todoNameEl.textContent = this._data.name;
         // todo - assign completed status
+        todoCheckboxEl.checked = this._data.completed;
 
-        todoCheckboxEl.id = `toso-${data.id}`;
+        todoCheckboxEl.id = `todo-${data.id}`;
         todoLabel.setAttribute("for", `todo-${data.id}`);
+
+        console.log(this._templateElement); // Check the template element
+        console.log(this._templateElement.content.querySelector(".todo")); // Check the .todo element
         
+
         return this._todoElement;
     }
 }
